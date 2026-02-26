d04e6c564a6fd8a098cacba06f8d6d11baa0aa20
Rahul Dandapat <dandapatrahul464@gmail.com>
2026-02-27 00:02:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if prompt := st.chat_input("Ask anything about Titanic passengers..."):
         llm = ChatGoogleGenerativeAI(
             model="gemini-2.5-flash",     
             temperature=0,
-            google_api_key=st.secrets["GEMINI_API_KEY"],
+            google_api_key=st.secrets["GEMINI_API_KEY"].strip('"'),
             streaming=True
         )
 
@@ -91,3 +91,4 @@ if prompt := st.chat_input("Ask anything about Titanic passengers..."):
 
 
         st.session_state.messages.append({"role": "assistant", "content": response})
+
